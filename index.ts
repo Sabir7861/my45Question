839daf59221c487ebd52d3364c5a387c5c85d3f9
Sabir Ali <162835315+Sabir7861@users.noreply.github.com>
2024-03-21 09:50:23 +0500

    Add files via upload

--- index.ts
+++ index.ts
@@ -1,135 +1,35 @@
-// Task 24
-// More conditional tests: you dont have to limit the number of tests to create to 10. if you want to try more compression,
-// Write more tests. more at least one true and one false result for each of the following 
-// Task for equality and unequality for strings 
-// let name_1 : string = "sidra"
-// let name_2 : string = "akram"
-// let name_3 : string = "miss sidra"l
-// if (name_1 == name_3){
-//     console.log("names are equal")
-// } else {
-//     console.log("names are not equal")
+// Task 34
+// pizzas: think of at least three kinds of your favourite pizza. store these pizza names in a array,then use for loop to print 
+// the name of each pizza 
+//. modify your for loop to print a sentence using the name of pizza instead of printing just the name of the pizza. foreach
+//pizza you should have one line of output containing a simple statement like I like pepperonal pizza. 
+//. add a line at the end of your programme, outside the for loop, that state how much you like pizza. the output should consist 
+// of three or more lines about the kinds of pizza you like and them in addational sentence, suchas really love pizza
+
+// let favourite_pizza : string [] = ["peppronal", "chicken","veg"]
+// for (let pizza of favourite_pizza) {
+//     console.log (pizza)
 // }
-// if (name_1 != name_2){
-//     console.log("names are equal")
-// } else {
-//     console.log("names are not equal")
-// }
+// console.log("/n")
 
-// let age_1 : number = 18
-// let age_2 : number = 27
-// if (age_1 == 18){
-//     console.log("eligible for vote")
-// }
-// if (age_2 == 27){
-//         console.log("eligible for vote in older category")
-//     }
-// if (age_1 <= age_2){
-//          console.log("younger")
-//      }
-//      if (age_2 <= age_1){
-//              console.log("older")
-        //  }
-        //  if (age_1 >= age_2){
-        //     console.log("younger")
-        // }
-        // if (age_1 <= age_2){
-        //     console.log("older")
-        // }
-
-        // if ("age_1 == 18 && age_2 == 27"){
-        //     console.log("person is eligible for vote ")
-        // }
-        // if ("age_1 == 18 || age_2 != 27") {
-        //     console.log("person is eligible not for vote ")
-        // }
-        // let country : string [] = ["pakistan","japan","china"]
-        // if (country.includes("pakistan")){
-        //     console.log("pakistan is in country list")
-        // }
-        // if (country.includes("japan")){
-        //         console.log("india is not include in an array")
-        //      }
-
-    //     // Task 25
-    //    let alien_colour : string = "green"
-    //     if (alien_colour == "green")
-    //     console.log('you earn 7 points')
-    // let alien_colour : string = "red"
-    // if (alien_colour == "red")
-    //   console.log('you earn 10 points')
-
-    // Task 26
-    // let alien_colour = "green"
-    //     if (alien_colour == "green"){
-    //      console.log('player just earn 5 points for shotting the alien ')  
-    //     } else {
-    //         console.log("player just earned 10 points ")
-    //     }
-        // let alien_colour = "red"
-        // if (alien_colour == "green"){
-        //  console.log('player just earn 5 points for shotting the alien ')  
-        // } else {
-        //     console.log("player just earned 10 points ")
-        // }
-
-        //Task 27
-        // let alien_colour : string = "yellow"
-        // if (alien_colour == "green"){
-        //     console.log("5 points")
-        // } else if (alien_colour == "yellow"){
-        //     console.log("10 points")
-        // } else  {
-        //     console.log("15 points")
-         // }
-
-      //Task 28 ........2,4,11,20,65,older
-// let age : number = 65
-// if (age < 2) {
-//     console .log ("you are a baby")
-// } else if (age < 4) {
-//     console.log("you are a toddler")
-// }else if (age < 11) {
-//     console.log("you are a kid")
-// } else if (age < 20) {
-//     console.log("you are a teenager")
-// } else if (age < 65) {
-//     console.log("you are a adult")
-// } else {
-//     console.log("you are a older")
+// for (let pizza of favourite_pizza) {
+//     console.log(`i really like ${pizza}pizza!`)
 // }
 
-// //Task 29 you realy like banana
-// let favourite_fruit : string [] = ["apple" ,"orange" ,"grapes",]
-// if (favourite_fruit.includes("apple")) {
-//     console.log("apple")
-// }
-//  if (favourite_fruit.includes("grapes")) {
-//     console.log("you realy like bananas")
+// Task 35
+//Animals: think of at least three different animals that have a common charateristic. store the name of these animals analist,
+//And they use for loop to print the name of each animal. .to modify a programme to print a statement about each animal, such 
+//As a dog would make a great pet. .add a line at the end of your programme starting what these animals have an common.you could print 
+// A statement such as any of these animals would make a great pet!
+
+// let animals : string [] = ["cat","lion","dog"]
+// for (let animal of animals) {
+//     console.log(animals)
 // } 
-// if (favourite_fruit. includes("orange")) {
-//     console.log("orange")
+//     console.log("/n")
+// for (let animal of animals) {
+//     console.log(` A ${animal} has a tell`)
 // }
-// if (favourite_fruit.includes("watermellon")) {
-// console.log("you realy like banana") 
-// } 
- 
-// if (favourite_fruit .includes("grapes")) {
-//     console.log("grapes")
-//  }
- 
-// Task 30
-// hello admain : make a array five are more usernames,including the name "admain". imagine your writing code will print 
-// greating to each user after the log into the website. loop throw the array and print a greating to each user
-// if user name "admain" print a special greating, such as hello admain, would you like to see a status report 
-// otherwise, print a generic greeting, such as hello eric, thank you for logging again
-//  let users: string[] = ["admain", "eric", "ali"]
-// for(let user of users) {
-//     if (user === "admain")
-//     console.log("hello admain, would you like to see a status report?")
-// }
-// {
-//     console.log ('hello $(user),thank you loging in again')
-// }
-
+// console.log("\n all of these are great pets but i love dogs more")
+    
 
